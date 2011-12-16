99c0648d79a8c7188dc760739a4a3fd47427ee02
Christopher Schmidt <christopher.schmidt@nokia.com>
2011-12-16 07:32:19 -0500

    Rewrite to support multiple actions

--- instances.py
+++ instances.py
@@ -1,8 +1,41 @@
 #!/usr/bin/python
 
 import boto.ec2
+import sys
 
-r = boto.ec2.connect_to_region("us-east-1")
-for reservation in r.get_all_instances():
-    for instance in reservation.instances:
-        print instance.id, instance.state, instance.ip_address, instance.key_name, instance.launch_time
+def connect():
+    r = boto.ec2.connect_to_region("us-east-1")
+    return r
+
+r = connect()
+def list():
+    """List active volumes"""
+    r = connect()
+    for reservation in r.get_all_instances():
+        for instance in reservation.instances:
+            print instance.id, instance.state, instance.ip_address, instance.key_name, instance.launch_time, "\n    ", instance.dns_name
+list.activity = True
+
+def console(instance_id):
+    r = connect()
+    i = r.get_all_instances(instance_id)[0].instances[0]
+    print i.get_console_output().output
+
+def terminate(instance_id):
+    """Delete a volume, given an instance id"""
+    r = connect()
+    i = r.get_all_instances(instance_id)[0]
+    return i.instances[0].terminate()
+terminate.activity = True
+
+def help():
+    for g in globals():
+        if hasattr(globals()[g], 'activity'):
+            print g, '-', globals()[g].__doc__
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if not args:
+        args = ['list']
+    ret = globals()[args[0]](*args[1:])
+    if ret != None: 
+        print ret
